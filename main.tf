841a933a5ab03932cb94c3da9b7fc085890b72e5
jbrunocas-design <jbrunocas@gmail.com>
2025-12-09 12:04:55 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "hello" {
+  ami           = "ami-0156001f0548e90b1" # Latest Amazon Linux 2 AMI for us-east-1
+  instance_type = "t3.micro"              # Free Tier eligible
+
+  tags = {
+    Name = "HelloWorldInstance"
+  }
+}
